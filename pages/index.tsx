e4009404b7564ca71b81d784a7a89bd3b4343536
嶋津　温紀 <attun@shimadzuatsushikinoMacBook-Air.local>
2021-05-24 13:18:59 +0900

    前準備2

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import type { NextPage } from "next";
 
+import Head from "next/head";
+
 import Button from "../components/Button";
 
 import { GetCovidData } from "../lib/posts";
@@ -8,13 +10,13 @@ import { GetCovidData } from "../lib/posts";
 const Index: NextPage = () => {
   console.log(GetCovidData());
 
-  // 副作用フック
-  React.useEffect(() => {
-    // mount時
-    alert("mouted");
-    // 副作用フック（アンマウント時）
-    return () => alert("cleanup");
-  }, []);
+  // // 副作用フック
+  // React.useEffect(() => {
+  //   // mount時
+  //   alert("mouted");
+  //   // 副作用フック（アンマウント時）
+  //   return () => alert("cleanup");
+  // }, []);
 
   // // 副作用フック（依存の変更時）
   // React.useEffect(()=>{
@@ -23,6 +25,10 @@ const Index: NextPage = () => {
 
   return (
     <>
+      <Head>
+        <title>My page title</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <h1>test</h1>
       <Button label="ボタンだよ" onClick={() => alert("押した")} />
     </>
